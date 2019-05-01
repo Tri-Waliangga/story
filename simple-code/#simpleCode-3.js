464b7f58b07926c0247c10a2db66f1de1524fa4b
Tri-Waliangga <49595468+Tri-Waliangga@users.noreply.github.com>
2019-05-01 19:36:49 +0700

    hapus comment.

--- simple-code/#simpleCode-3.js
+++ simple-code/#simpleCode-3.js
@@ -1,5 +1,5 @@
 const berangkat = (status) => {
   return status == 'wajib' ? 'Males, tetap berangkat.' : 'Males, ga berangkat';
 }
-//Semales apapun, kewajiban tetap harus dilaksanakan. Namanya juga kewajiban, hehehe...
+
 console.log(berangkat('wajib'));
