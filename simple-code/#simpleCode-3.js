c20a7465c1a9f114385fd26009195490101e43a2
Tri-Waliangga <49595468+Tri-Waliangga@users.noreply.github.com>
2019-05-07 02:12:59 +0700

    menambahkan comment dan judul

--- simple-code/#simpleCode-3.js
+++ simple-code/#simpleCode-3.js
@@ -1,5 +1,8 @@
 const berangkat = (status) => {
   return status == 'wajib' ? 'Males, tetap berangkat.' : 'Males, ga berangkat';
 }
+//Kalau ga wajib, ga berangkat saya...
 
 console.log(berangkat('wajib'));
+
+//'Third Chapter - Senin, berangkat - Second week'
