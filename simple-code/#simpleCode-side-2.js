3fdd53aaa4f6c7ed1d77d92adb431b672c2af082
Tri-Waliangga <49595468+Tri-Waliangga@users.noreply.github.com>
2019-05-07 02:32:31 +0700

    membuat simpleCode-side-2

---
+++ simple-code/#simpleCode-side-2.js
@@ -0,0 +1,12 @@
+let goal = (kondisi) => {
+  while(kondisi == 'berjuang') {
+    console.log('harapan');
+  }
+  
+  if(kondisi == 'putus-asa')
+    return 'Semuanya berakhir tanpa ada akhir'
+}
+
+console.log(goal('berjuang'))
+
+//'Side Chapter - Perjuangan, sekecil apapun masih ada harapan'
